4175dab36a122d98cd4bc28e912944860f94272a
varun <varun-kumar@live.com>
2020-09-24 20:02:15 +0530

    implemented upper and lower bound

--- searching/binary_search.cpp
+++ searching/binary_search.cpp
@@ -23,9 +23,53 @@ int search(vector<int> v, int target)
   return -1;
 }
 
+int findUpperBound(vector<int> v, int target)
+{
+  int L = 0, R = v.size() - 1;
+  int ans = -1;
+
+  while (L <= R)
+  {
+    int mid = L + (R - L) / 2;
+
+    if (v[mid] > target)
+    {
+      ans = mid;
+      R = mid - 1;
+    }
+    else
+    {
+      L = mid + 1;
+    }
+  }
+  return ans;
+}
+
+int findLowerBound(vector<int> v, int target)
+{
+  int L = 0, R = v.size() - 1;
+  int ans = -1;
+
+  while (L <= R)
+  {
+    int mid = L + (R - L) / 2;
+
+    if (v[mid] < target)
+    {
+      ans = mid;
+      L = mid + 1;
+    }
+    else
+    {
+      R = mid - 1;
+    }
+  }
+  return ans;
+}
+
 int main()
 {
-  vector<int> vec{-1, 2, 3, 5, 7, 8, 10, 23, 345};
-  int key = 5;
-  cout << search(vec, key);
+  vector<int> vec{2, 3, 5, 6, 8, 10, 12};
+  int key = 4;
+  cout << findUpperBound(vec, key);
 }
