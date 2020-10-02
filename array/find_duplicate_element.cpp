86333b4576722aeb94e79c7b0daf545f7b5759d9
varun <varun-kumar@live.com>
2020-10-02 19:10:26 +0530

    added algo for finding duplicate element

---
+++ array/find_duplicate_element.cpp
@@ -0,0 +1,42 @@
+/*
+	Problem link - https://leetcode.com/problems/find-the-duplicate-number/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Navive approch {Time: O(nlogn), space: O(1)}
+int findDuplicateNumberNavive(vector<int> v)
+{
+	int ans;
+	// sort the array and traverse to find duplicate element
+	sort(v.begin(), v.end());
+	int prev = v[0];
+	for (int i = 1; i < v.size(); i++)
+	{
+		if (v[i] == prev)
+		{
+			ans = prev;
+			break;
+		}
+		else
+		{
+			prev = v[i];
+		}
+	}
+	return ans;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	// for getting input from input.txt
+	freopen("input1.txt", "r", stdin);
+	// for writing output to output.txt
+	freopen("output1.txt", "w", stdout);
+#endif
+	vector<int> t1{1, 3, 4, 2, 2};
+	vector<int> t2{3, 1, 3, 4, 2};
+
+	cout << findDuplicateNumberNavive(t1);
+}
