bc19454e2c0d15aea168c9b20931150f53842de3
varun <varun-kumar@live.com>
2020-10-17 06:43:39 +0530

    optimized union

--- array/union_of_two_arrays.cpp
+++ array/union_of_two_arrays.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> find_union(vector<int> v1, vector<int> v2)
+int find_union(vector<int> v1, vector<int> v2)
 {
-	set<int> s;
+	unordered_set<int> s;
 	vector<int> ans;
 	for (auto const x : v1)
 	{
@@ -13,20 +13,12 @@ vector<int> find_union(vector<int> v1, vector<int> v2)
 	{
 		s.insert(x);
 	}
-	for (auto const e : s)
-	{
-		ans.push_back(e);
-	}
-	return ans;
+	return s.size();
 }
 
 int main()
 {
 	vector<int> v1{85, 25, 1, 32, 54, 6};
 	vector<int> v2{85, 2};
-	vector<int> union_array = find_union(v1, v2);
-	for (auto const x : union_array)
-	{
-		cout << x << " ";
-	}
+	cout << find_union(v2,v2);
 }
